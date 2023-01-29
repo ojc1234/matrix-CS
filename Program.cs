efe37c38c23fb19ec46de3c7e891af9ec5d6ec1b
ojc1234 <54547113+ojc1234@users.noreply.github.com>
2023-01-29 23:35:27 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace martixDiscord
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var matrix2D = new Matrix(new List<int> { 1, 2 },new List<int> { 3, 4 });
+            var tempMat = matrix2D.Deepcopy();
+            matrix2D.Write();
+            matrix2D.Scalar(3);
+            matrix2D.Write();
+            matrix2D.MatrixAdd(tempMat);
+            matrix2D.Write();
+
+        }
+    }
+}
